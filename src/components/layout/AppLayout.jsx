438395335f5de39d355fe7e064b67a8a4beed57e
ThomasScapini <scapinithomas1997@gmail.com>
2026-03-24 15:23:24 +0100

    fix: overflow orizzontale layout

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,9 +4,9 @@ import MobileNav from "./MobileNav";
 
 export default function AppLayout() {
   return (
-    <div className="flex min-h-screen bg-background">
+    <div className="flex min-h-screen bg-background" style={{ overflowX: "hidden", maxWidth: "100vw" }}>
       <Sidebar />
-      <main className="flex-1 flex flex-col min-h-screen">
+      <main className="flex-1 flex flex-col min-h-screen" style={{ minWidth: 0, overflow: "hidden" }}>
         <Outlet />
       </main>
       <MobileNav />
